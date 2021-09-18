3593c6d37586562838f55e9280ee8b69881fd50a
Dmitry Khodakovsky <khda91@gmail.com>
2021-09-19 01:49:58 +0300

    logger check commit

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -2,15 +2,20 @@ package org.example;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import java.util.logging.Logger;
 import org.testng.annotations.Test;
 
 public class SampleTest {
 
+    private static final Logger LOGGER =  Logger.getLogger(SampleTest.class.getName());
+
     @Test
     public void test() {
         Configuration.headless = true;
+        LOGGER.info("Тест начался");
         Selenide.open("https://apparel-uk.local:9002/ucstorefront/");
         Selenide.screenshot("sample.png");
         Selenide.closeWebDriver();
+        LOGGER.info("Тест завершился");
     }
 }
